c59601a4f703189bbd71d4d37d2313d66fa63309
Yi Lian <yi.lian@mail.mcgill.ca>
2024-12-04 05:40:01 +0000

    version 1.2.1

--- R/auxiliary_functions.R
+++ R/auxiliary_functions.R
@@ -2,7 +2,7 @@
 l_ld <- function(beta, x, n, Start, Stop, Event){
   fit.cox <- coxph(Surv(Start, Stop, Event) ~ x,
                    init = beta,
-                   control = list('iter.max' = 0, timefix = FALSE))
+                   control = coxph.control(iter.max = 0, timefix = FALSE))
   l <- - fit.cox$loglik[1]/n
   ld <- - fit.cox$first/n
   return(c(l, ld))
